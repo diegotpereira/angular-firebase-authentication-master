ae0eaeaac5d6dc15dc45b2fd8a88abad4f9d0ea7
Diego <diegoteixeirapereira@hotmail.com>
2021-10-08 10:19:33 -0300

    alteração no arquivo de serviço usuario.

--- src/app/compartilhado/servico/auth.service.ts
+++ src/app/compartilhado/servico/auth.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, NgZone } from '@angular/core';
-import { Usuario } from '../servico/usuario';
+import { User }  from '../servico/user';
 import { auth } from 'firebase/app';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/firestore';
@@ -10,7 +10,7 @@ import { Router } from '@angular/router';
 })
 export class AuthService {
   usuarioDado: any; // Salvar dados do usuário conectado
-  
+ 
 
   constructor(
     public afs: AngularFirestore, // Injetar serviço Firestore
@@ -21,14 +21,14 @@ export class AuthService {
 
     // Salvar dados do usuário no armazenamento local quando
     // conectado e configurando nulo quando desconectado
-    this.afAuth.authState.subscribe(usuario => {
-      if (usuario) {
-        this.usuarioDado = usuario;
-        localStorage.setItem('usuario', JSON.stringify(this.usuarioDado));
-        JSON.parse(localStorage.getItem('usuario') !);
+    this.afAuth.authState.subscribe(user => {
+      if (user) {
+        this.usuarioDado = user;
+        localStorage.setItem('user', JSON.stringify(this.usuarioDado));
+        JSON.parse(localStorage.getItem('user') !);
       } else {
-        localStorage.setItem('usuario', '');
-        JSON.parse(localStorage.getItem('usuario') !);
+        localStorage.setItem('user', '');
+        JSON.parse(localStorage.getItem('user') !);
       }
     })
   }
@@ -41,7 +41,7 @@ export class AuthService {
        this.ngZone.run(() => {
          this.router.navigate(['painel']);
        });
-      //  this.definirDadosUsuario(result.usuario);
+       this.definirDadosUsuario(result.user);
      }).catch((error: { message: any; }) => {
        window.alert(error.message)
      })
@@ -54,26 +54,54 @@ export class AuthService {
        // Chame a função SendVerificaitonMail () quando o novo usuário assinar
        // e retorna promise
        this.EnviarMensagemDeVerificacao();
-      //  this.definirDadosUsuario(result.usuario);
+       this.definirDadosUsuario(result.user);
      }).catch((error: { message: any; }) => {
        window.alert(error.message)
      })
    }
 
    // Enviar verificação de e-mail quando um novo usuário se inscrever
+   EnviarMensagemDeVerificacao() {
+    // return this.afAuth.currentUser.then((user) => {
+    //   if (user != null) {
+    //     return user.sendEmailVerification();
+        
+    //   }
+      
+    // }).then(() => {
+    //   this.router.navigate(['verify-email-address']);
+    // })
+  }
   //  SendVerificationMail() {
-  //   return this.afAuth.currentUser.then((user) => {
-  //     return user.sendEmailVerification();
-  //   }).then(() => {
+  //   return this.afAuth.currentUser.sendEmailVerification()
+  //   .then(() => {
   //     this.router.navigate(['verify-email-address']);
   //   })
-  // }   
-   EnviarMensagemDeVerificacao() {
-    //  return this.afAuth.currentUser.sendEmailVerification()
-    //  .then(() => {
-    //    this.router.navigate(['verifique-endereco-email']);
-    //  })
-   }
+  // }
+  //  async SendVerificationMail() {
+  //   (await this.afAuth.currentUser).sendEmailVerification()
+  //   .then(() => {
+  //     this.router.navigate(['verify-email']);
+  //   })   }
+  //  SendVerificationMail() {
+  //   return this.afAuth.currentUser.sendEmailVerification()
+  //   .then(() => {
+  //     this.router.navigate(['verify-email']);
+  //   })
+  // }
+  //  EnviarMensagemDeVerificacao() {
+  //   return this.afAuth.currentUser.sendEmailVerification()
+  //   // return this.afAuth.currentUser.then(u => u.sendEmailVerification())
+  //   // .then(() => {
+  //   //   this.router.navigate(['verify-email']);
+  //   // })
+  // }  
+  //  EnviarMensagemDeVerificacao() {
+  //    return this.afAuth.currentUser.sendEmailVerification()
+  //    .then(() => {
+  //      this.router.navigate(['verifique-endereco-email']);
+  //    })
+  //  }
 
    // Redefinir senha esquecida
    RedefinirSenha(senhaDeRedefinicaoDeEmail: any) {
@@ -87,8 +115,8 @@ export class AuthService {
 
    // Retorna verdadeiro quando o usuário está conectado e o e-mail é verificado
    get estaLogado(): boolean {
-     const usuario = JSON.parse(localStorage.getItem('usuario') !);
-     return (usuario !== null && usuario.emailVerificado !== false) ? true : false;
+     const user = JSON.parse(localStorage.getItem('user') !);
+     return (user !== null && user.emailVerificado !== false) ? true : false;
    }
 
    // Faça login no Google
@@ -107,7 +135,7 @@ export class AuthService {
        this.ngZone.run(() => {
          this.router.navigate(['painel']);
        })
-      //  this.definirDadosUsuario(result.usuario);
+       this.definirDadosUsuario(result.user);
      }).catch((error: any) => {
        window.alert(error)
      })
@@ -116,14 +144,15 @@ export class AuthService {
    // Configurando dados do usuário ao fazer login com nome de usuario/senha,
    // inscreva-se com nome de usuário/senha e faça login com autenticação social
    // provedor no banco de dados Firestore usando o serviço AngularFirestore + AngularFirestoreDocument
-   definirDadosUsuario(usuario: { uid: any; email: any; nomeEmExibicao: any; fotoUrl: any; emailVerificado: any; }) {
-     const usuarioRef: AngularFirestoreDocument<any> = this.afs.doc(`usuarios/${usuario.uid}`);
-     const usuarioDado: Usuario = {
-       uid: usuario.uid,
-       email: usuario.email,
-       nomeEmExibicao: usuario.nomeEmExibicao,
-       fotoUrl: usuario.fotoUrl,
-       emailVerificado: usuario.emailVerificado
+   
+   definirDadosUsuario(user) {
+     const usuarioRef: AngularFirestoreDocument<any> = this.afs.doc(`users/${user.uid}`);
+     const usuarioDado: User = {
+       uid: user.uid,
+       email: user.email,
+       nomeEmExibicao: user.nomeEmExibicao,
+       fotoUrl: user.fotoUrl,
+       emailVerificado: user.emailVerificado
      }
      return usuarioRef.set(usuarioDado, {
        merge: true
@@ -133,7 +162,7 @@ export class AuthService {
    // Sair
    Sair() {
      return this.afAuth.signOut().then(() => {
-       localStorage.removeItem('usuario');
+       localStorage.removeItem('user');
        this.router.navigate(['entrar']);
      })
    }
--- src/app/compartilhado/servico/user.ts
+++ src/app/compartilhado/servico/user.ts
@@ -1,7 +1,8 @@
-export interface Usuario {
+export interface User {
     uid: string;
     email: string;
     nomeEmExibicao: string;
     fotoUrl: string;
     emailVerificado: boolean;
 }
+
